d34a38df04ebecbc43a3a1c184eb98eacec6c2c6
shadatshohan <shadatshohan5@gmail.com>
2025-02-26 12:35:40 +0600

    JSON_importing_first

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState,useEffect } from "react";
+import Form from "./Form";
+import List from "./List";
+
+function App() {
+  const API_URL='https://jsonplaceholder.typicode.com/';
+  const [reqType,setReqType]=useState('users');
+  const [items,setItems]=useState([]);
+
+  useEffect(()=>{
+    const fetchItems=async ()=>{
+      try{
+        const response=await fetch(`${API_URL}${reqType}`);
+        const data=await response.json();
+        console.log(data)
+        setItems(data);
+      }
+      catch(err)
+      {
+        console.log(err);
+      }
+    }
+    fetchItems();
+  },[reqType])
+  return (
+    <div className="App">
+      <Form reqType={reqType} setReqType={setReqType}/>
+      <List items={items}/>
+    </div>
+  );
+}
+
+export default App;
